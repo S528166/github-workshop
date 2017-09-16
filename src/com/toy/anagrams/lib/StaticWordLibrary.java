e3176622acbe04a47b64561467ceb56bc145ca0c
s528744 <s528744@nw39497.nwmissouri.edu>
2017-09-15 22:38:13 -0500

    Added scramble word in the staticwordlibrary.java

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -37,6 +37,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
+        "abhilash",
         "abstraction",
         "ambiguous",
         "arithmetic",
@@ -85,6 +86,7 @@ final class StaticWordLibrary extends WordLibrary {
         "abhilash"};
 
     private static final String[] SCRAMBLED_WORD_LIST = {
+        "hsalihba",
         "batsartcoin",
         "maibuguos",
         "ratimhteci",
